bafb9cf17013523f40d8cd0c8c35f84296cb9c4f
kamleshp95 <kamleshpadwal@gmail.com>
2024-04-03 18:45:53 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="kamlesh",
+    author_email="kamleshpadwal@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
